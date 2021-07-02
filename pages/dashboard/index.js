1fe1b6bde955ab44aefc4fbdafc0e94b54c62634
GianJie <gianjie.choo@emporioanalytics.com>
2021-07-02 16:19:06 +0800

    camera

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from '../../styles/Home.module.css';
 import Link from 'next/link'
 
 export default function Home() {
 
- const onClick = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: true
-    })
-    const videoTracks = stream.getVideoTracks()
-    const track = videoTracks[0]
-    alert(`Getting video from: ${track.label}`)
-    document.querySelector('video').srcObject = stream
-    document.querySelector('#get-access').setAttribute('hidden', true)
-    //The video stream is stopped by track.stop() after 3 second of playback.
+  useEffect(() => {
+    var video = document.createElement('video');
+    video.style.width = 200 + 'px';
+    video.style.height = 200 + 'px';
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsinline', '');
+  }, []);
 
   
-  } catch (error) {
-    alert(`${error}`)
-    console.error(error)
+  const onClick = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          facingMode: 'environment'
+        }
+      })
+      const videoTracks = stream.getVideoTracks()
+      // const track = videoTracks[0]
+
+      document.querySelector('video').srcObject = stream
+      document.querySelector('video').style.width = window.outerWidth + 'px';
+      document.querySelector('video').style.height = window.outerHeight + 'px';
+      document.querySelector('video').setAttribute('autoplay', '');
+      document.querySelector('video').setAttribute('muted', '');
+      document.querySelector('video').setAttribute('playsinline', '');
+      document.querySelector('#get-access').setAttribute('hidden', true)
+      //The video stream is stopped by track.stop() after 3 second of playback.
+
+    
+    } catch (error) {
+      alert(`${error}`)
+      console.error(error)
+    }
   }
- }
 
   return (
     <div className={styles.container}>
@@ -35,7 +52,7 @@ export default function Home() {
           </Link>
         </h1>
         <button id="get-access" onClick={onClick}>Get access to camera</button>
-        <video autoPlay></video>
+        <video/>
       </main>
     </div>
   );

f9663faa85b15ae5c52847f645a4f9282f475577
GianJie <gianjie.choo@emporioanalytics.com>
2021-07-02 17:41:35 +0800

    test prototype

--- pages/camera/index.js
+++ pages/camera/index.js
@@ -1,19 +1,36 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from '../../styles/Home.module.css';
+import Link from 'next/link'
 
+const style = {
+  button : {
+    zIndex: 1, width: 60, height: 60, borderRadius: 40,
+    border: 'none', position:"fixed", bottom: 50, backgroundColor:"white"
+  },
+  XContainer: {
+    position:'absolute', top: 20, left: 25, height: 50, width: 50
+  },
+  SubmitContainer: {
+    position:'absolute', bottom: 0, left: 0, right: 0,
+    display:'flex', justifyContent: 'center',
+  }
+}
 export default function Home() {
+
+  const [takePhoto, setTakePhoto] = useState(null);
 
   useEffect(() => {
     setupCamera()
   }, []);
 
-  
+  const reTakePhoto = () => setTakePhoto(null);
+
   const captureVideo = (video) => {
     var canvas = document.createElement("canvas");
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     var canvasContext = canvas.getContext("2d");
-    canvasContext.drawImage(video, 0, 0);
+    canvasContext.drawImage(video, 0, 50);
     return canvas.toDataURL('image/png');
   }
 
@@ -30,47 +47,14 @@ export default function Home() {
 
       document.querySelector('video').srcObject = stream
       document.querySelector('video').style.position = 'fixed';
-      document.querySelector('video').style.height = window.outerHeight + 'px';
+      document.querySelector('video').style.height = window.outerHeight + 'px'
       // document.querySelector('video').style.width = window.outerWidth + 'px';
       // document.querySelector('#get-access').setAttribute('hidden', true)
       //The video stream is stopped by track.stop() after 3 second of playback.
 
-      const mediaStreamTrack = stream.getVideoTracks()[0];
-      const imageCapture = new ImageCapture(mediaStreamTrack);
-
-      await imageCapture.getPhotoCapabilities()
-      
-      const btn = document.querySelector('.switch');
-      btn.addEventListener('click', function(){
-        try {
-          mediaStreamTrack.applyConstraints({
-            advanced: [{torch: true}]
-          });
-        } catch (e) {
-          alert(`${e}`)
-          console.log(e)
-        }
-      });
-      //Create image capture object and get camera capabilities
-      // const imageCapture = new ImageCapture(track)
-      // imageCapture.getPhotoCapabilities().then(() => {
-      //   try {
-      //     //todo: check if camera has a torch
-      //     //let there be light!
-      //     const btn = document.querySelector('.switch');
-      //     btn.addEventListener('click', function(){
-      //       track.applyConstraints({
-      //         advanced: [{torch: true}]
-      //       });
-      //     });
-      //   } catch (err) {
-      //     console.log(err)
-      //   }
-      // });
-
-      document.querySelector('video').addEventListener('click', function() {
+      document.querySelector('button').addEventListener('click', function() {
         const blobImage64 = captureVideo(document.querySelector('video'));
-        console.log({blobImage64})
+        setTakePhoto(blobImage64)
       });
       
     } catch (error) {
@@ -78,12 +62,33 @@ export default function Home() {
       console.error(error)
     }
   }
-
+ 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <video autoPlay muted={true} playsInline/>
-        <button className="switch" style={{zIndex:1}}>On / Off</button>
+        <video autoPlay muted playsInline/>
+        <div style={{zIndex:1}}>
+          <span style={{height: 50, width: 50, borderRadius: 25, backgroundColor:"red"}}></span>
+        </div>
+        <button style={style.button}/>
+        {takePhoto && 
+          <div style={{zIndex: 2, position:"fixed", backgroundColor:'black'}}>
+            <div style={style.XContainer} onClick={reTakePhoto}>
+              <span style={{color:'white', fontSize: 25}}>X</span>
+            </div>
+            <img 
+              style={{
+                width: window?.outerWidth,
+                height: window?.outerHeight,
+                objectFit:'contain'
+              }} 
+              src={takePhoto}
+            />
+            <div style={style.SubmitContainer} onClick={reTakePhoto}>
+              <span style={{color:'white', fontSize: 15, border: '1px solid white', padding: '2.5px 10px'}}>Submit</span>
+            </div>
+          </div>
+        }
       </main>
     </div>
   );

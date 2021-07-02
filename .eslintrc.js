fc744c04aea2cc4e5c1abacfe3d1115e3231eaff
GianJie <gianjie.choo@emporioanalytics.com>
2021-07-02 15:14:29 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
         "plugin:react/recommended",
         "next",
         "next/core-web-vitals",
-        "prettier"
     ],
     "parserOptions": {
         "ecmaFeatures": {
